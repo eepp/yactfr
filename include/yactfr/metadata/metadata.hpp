51f6319e126ed0ea1bf5e3314e6df88dde1aecab
Philippe Proulx <eeppeliteloop@gmail.com>
2018-06-06 10:35:47 -0400

    doc: add note about traceTypeFromMetadataText()
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- include/yactfr/metadata/metadata.hpp
+++ include/yactfr/metadata/metadata.hpp
@@ -58,6 +58,21 @@ private:
 @brief  Builds a trace type object by parsing the metadata text from
         \p begin to \p end.
 
+Because this template uses another, quite heavy template, it is
+recommended, if you are to use this function using the same
+\p CharIt template parameter in different translation units, that you
+create a non-templated, specialized version in its own source file,
+for example:
+
+@code
+yactfr::TraceType::SP traceTypeFromMetadataText(const std::string& str,
+                                                const boost::optional<boost::uuids::uuid>& expectedUuid)
+{
+    return yactfr::traceTypeFromMetadataText(std::begin(str), std::end(str),
+                                             expectedUuid);
+}
+@endcode
+
 @ingroup metadata
 
 @tparam CharIt      Random access character iterator type.
