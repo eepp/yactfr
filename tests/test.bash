24a7081f313a1311d063ec0671779a2889a4df32
Philippe Proulx <eeppeliteloop@gmail.com>
2018-10-10 20:30:00 -0400

    Allow `make check` to be issued from any build directory
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/test.bash
+++ tests/test.bash
@@ -5,6 +5,13 @@
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE file for details.
 
+build_dir="$1"
+
+export bats_bin="$(pwd)/bats/bin/bats"
+export build_tests_path="$build_dir/tests"
+export testers_path="$build_tests_path/testers"
+
+rc=0
 test_dirs=(
   tests-metadata-text
   tests-metadata-stream
@@ -13,12 +20,6 @@ test_dirs=(
   tests-iter-pos
   tests-pkt-seq
 )
-
-export bats_bin="$(pwd)/bats/bin/bats"
-export build_tests_path="$(pwd)/../build/tests"
-export testers_path="$build_tests_path/testers"
-
-rc=0
 
 for d in ${test_dirs[@]}; do
   pushd $d > /dev/null
